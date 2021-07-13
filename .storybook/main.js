f03cccf37da98a6b90b8e52c0c7aa58a0dda35c2
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 00:03:53 +0800

    init(storybook): introduce PostCSS 8 support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss")
+        }
+      }
+    }
   ]
-}
+};
