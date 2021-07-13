734d804971b3ab1b0291c1701a25164623e9f0a2
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-13 23:50:31 +0800

    refactor(components/Card/CourseInfoCard): tweak the design

--- components/Card/CourseInfoCard.tsx
+++ components/Card/CourseInfoCard.tsx
@@ -28,19 +28,21 @@ export default function CourseInfoCard({
 }: CourseInfoCardProps) {
   return (
     <BaseInfoCard
-      className="flex items-end content-end bg-cover"
+      className="bg-cover"
       style={{
         backgroundImage: `url(${backgroundImage})`,
       }}
     >
-      <div className="p-8 bg-black bg-opacity-60 text-white backdrop-filter backdrop-blur-sm">
-        <div className="mb-1">
-          {tags.map((tag) => (
-            <CourseInfoTags key={`${title}-tag-${tag}`}>{tag}</CourseInfoTags>
-          ))}
+      <div className="p-8 bg-black bg-opacity-60 text-white flex items-end content-end w-full h-full">
+        <div>
+          <div className="mb-1">
+            {tags.map((tag) => (
+              <CourseInfoTags key={`${title}-tag-${tag}`}>{tag}</CourseInfoTags>
+            ))}
+          </div>
+          <div className="text-xl font-bold mb-1">{title}</div>
+          <div className="text-sm">{desc}</div>
         </div>
-        <div className="text-xl font-bold mb-1">{title}</div>
-        <div className="text-sm">{desc}</div>
       </div>
     </BaseInfoCard>
   );
