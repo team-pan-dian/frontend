7c286be6a6337c6d60b55ec43295caeaf57ca49a
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 01:06:02 +0800

    feat(components/Card/BaseCard): docs

--- components/Card/BaseCard.tsx
+++ components/Card/BaseCard.tsx
@@ -5,6 +5,9 @@ export interface BaseCardProps {
   children: ReactNode;
 }
 
+/**
+ * 基礎通用卡片。
+ */
 export default function BaseCard({ children }: BaseCardProps) {
   return (
     <div className="overflow-hidden rounded-lg shadow-lg main-content md:w-max standard-color">
