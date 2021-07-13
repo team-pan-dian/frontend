5af5f73b981fa56c80835d738e77f8b940cab67c
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 02:23:31 +0800

    feat(components/Page/BasePage/story): done

---
+++ components/Page/BasePage.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import type { Meta, Story } from "@storybook/react";
+import type { BasePageProps } from "./BasePage";
+import BasePage from "./BasePage";
+
+export default {
+  title: "Page/BasePage",
+  component: BasePage,
+} as Meta;
+
+export const BasePageExample: Story<BasePageProps> = ({
+  id,
+  title,
+  full,
+  children = <p>Hello</p>,
+}: BasePageProps) => (
+  <BasePage id={id} title={title} full={full}>
+    {children}
+  </BasePage>
+);
+
+BasePageExample.args = {
+  id: "base-page-example",
+  title: "Base Page Example",
+};
