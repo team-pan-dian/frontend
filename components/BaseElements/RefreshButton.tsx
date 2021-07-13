4f299ab88eb13a19ce565584b2a13e5cc92179d9
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 00:19:03 +0800

    docs(components/BaseElements/RefreshButton): add docs

--- components/BaseElements/RefreshButton.tsx
+++ components/BaseElements/RefreshButton.tsx
@@ -5,6 +5,9 @@ export interface RefreshButtonProps {
   className?: string;
 }
 
+/**
+ * 呼叫 `location.reload()` 的重新整理按鈕。等同於 Command-R 和 F5。
+ */
 export default function RefreshButton({ className = "" }: RefreshButtonProps) {
   return (
     <BaseButton
