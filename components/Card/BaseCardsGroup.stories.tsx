83e247523c54feec1fff546ae4336b761a43aa20
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 18:27:34 +0800

    feat(components/Card/BaseCardsGroup/story): update examples

--- components/Card/BaseCardsGroup.stories.tsx
+++ components/Card/BaseCardsGroup.stories.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import type { BaseCardsGroupProps } from "./BaseCardsGroup";
 import BaseCardsGroup from "./BaseCardsGroup";
 import CourseInfoCard from "./CourseInfoCard";
-import { ExampleCard1 } from "./CourseInfoCard.stories";
+import { DarkSingleTag, LightMultipleTags } from "./CourseInfoCard.stories";
 
 export default {
   title: "Card/BaseCardsGroup",
@@ -15,12 +15,29 @@ const Template: Story<BaseCardsGroupProps> = ({
   children,
 }: BaseCardsGroupProps) => <BaseCardsGroup>{children}</BaseCardsGroup>;
 
-const ExampleCard = () => {
-  if (ExampleCard1.args) {
-    const { title, desc, tags, backgroundImage } = ExampleCard1.args;
+const DarkSingleTagCard = () => {
+  if (DarkSingleTag.args) {
+    const { title, desc, tags, backgroundImage } = DarkSingleTag.args;
     if (title && desc && tags && backgroundImage)
       return (
-        <ExampleCard1
+        <DarkSingleTag
+          title={title}
+          desc={desc}
+          tags={tags}
+          backgroundImage={backgroundImage}
+        />
+      );
+  }
+
+  return null;
+};
+
+const LightMultipleTagsCard = () => {
+  if (LightMultipleTags.args) {
+    const { title, desc, tags, backgroundImage } = LightMultipleTags.args;
+    if (title && desc && tags && backgroundImage)
+      return (
+        <DarkSingleTag
           title={title}
           desc={desc}
           tags={tags}
@@ -36,8 +53,11 @@ export const Example = Template.bind({});
 Example.args = {
   children: (
     <>
-      {new Array(30).fill("card").map((data, cardId) => (
-        <ExampleCard key={`${data}-${cardId - 1}`} />
+      {new Array(15).fill("card").map((data, cardId) => (
+        <DarkSingleTagCard key={`${data}-${cardId - 1}`} />
+      ))}
+      {new Array(15).fill("card").map((data, cardId) => (
+        <LightMultipleTagsCard key={`${data}-${cardId - 1}`} />
       ))}
     </>
   ),
