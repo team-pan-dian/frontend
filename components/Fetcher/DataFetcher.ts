4bd2a7c33236d5dc04c1a4aeeaf38df2e12d1c13
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-15 07:56:10 +0800

    feat(components/Fetcher/DataFetcher): done

--- components/Fetcher/DataFetcher.ts
+++ components/Fetcher/DataFetcher.ts
@@ -1,9 +1,40 @@
+import type { AnyType, Infer } from "myzod";
+import myzod from "myzod";
+
+export const BasicResponseSchema = myzod.object({
+  error: myzod.boolean(),
+  errorMessage: myzod.string(),
+});
+
 export default async function DataFetcher(path: string) {
   const endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
-  if (typeof endpoint === "string")
+  if (typeof endpoint !== "string")
     throw new Error(
       "You should specify the value to NEXT_PUBLIC_API_ENDPOINT environment variable."
     );
 
-  return fetch(`${endpoint}/${path}`);
+  const responseData = await fetch(`${endpoint}/${path}`);
+  return responseData.json();
+}
+
+export function ResponseParser<T extends AnyType>(
+  data: unknown,
+  error: unknown,
+  schema: T
+): { data: Infer<T> | null; error: null } | { data: null; error: Error } {
+  try {
+    if (error) throw error;
+    if (data) {
+      const typedData = schema.parse(data) as Infer<T>;
+      return { data: typedData, error: null };
+    }
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return { data: null, error: e };
+    }
+
+    return { data: null, error: new Error(`${e}`) };
+  }
+
+  return { data: null, error: null };
 }
