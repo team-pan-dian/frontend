e24c38d0efb5a3b0ec528f39bcfbd00c76d2d179
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-15 07:40:47 +0800

    feat(components/Combination/Message): done

---
+++ components/Combination/Message.tsx
@@ -0,0 +1,42 @@
+import type { ReactNode } from "react";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faExclamationTriangle,
+  faInfoCircle,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
+
+export enum MessageLevel {
+  INFO = "info",
+  WARN = "warn",
+  ERROR = "error",
+}
+
+const MessageLevelClass: Record<MessageLevel, string> = {
+  info: "bg-blue-500 text-white",
+  warn: "bg-yellow-500 text-white",
+  error: "bg-red-500 text-white",
+};
+
+const MessageLevelIcon: Record<MessageLevel, ReactNode> = {
+  info: <FontAwesomeIcon icon={faInfoCircle} />,
+  warn: <FontAwesomeIcon icon={faExclamationTriangle} />,
+  error: <FontAwesomeIcon icon={faTimesCircle} />,
+};
+
+export interface ErrorMessageProps {
+  errorMessage: string;
+  mode: MessageLevel;
+}
+
+export default function Message({ errorMessage, mode }: ErrorMessageProps) {
+  return (
+    <div
+      className={`flex w-full space-x-2 items-start p-4 ${MessageLevelClass[mode]}`}
+    >
+      <div>{MessageLevelIcon[mode]}</div>
+      <div>{errorMessage}</div>
+    </div>
+  );
+}
