d57ec442ec7a294cfb5454ecde0d5e8727ebd444
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 11:24:44 +0800

    feat(components/Jumbotron/SelectSearchCourse/story): done

---
+++ components/Jumbotron/SelectSearchCourse.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import type { Meta, Story } from "@storybook/react";
+import SelectSearchCourse from "./SelectSearchCourse";
+import JumbotronBase from "./JumbotronBase";
+
+export default {
+  title: "Jumbotron/SelectSearchCourse",
+  component: SelectSearchCourse,
+  subcomponents: {
+    JumbotronBase,
+  },
+} as Meta;
+
+export const NormalForm: Story = () => (
+  <JumbotronBase>
+    <SelectSearchCourse />
+  </JumbotronBase>
+);
