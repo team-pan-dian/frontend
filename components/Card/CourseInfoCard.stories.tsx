847c8a63724e50c7c085bce24eb5f6522f6fdfda
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 13:04:15 +0800

    feat(components/Card/CourseInfoCard): add more examples

--- components/Card/CourseInfoCard.stories.tsx
+++ components/Card/CourseInfoCard.stories.tsx
@@ -24,12 +24,42 @@ const Template: Story<CourseInfoCardProps> = ({
   </div>
 );
 
-export const ExampleCard1 = Template.bind({});
-ExampleCard1.args = {
+export const DarkSingleTag = Template.bind({});
+DarkSingleTag.args = {
   title: "沒有駭客學校，但你可以自學。",
   desc: "這堂課程可以讓您從零基礎，搖身一遍成為資安高手。",
   tags: ["資訊"],
   // source: https://unsplash.com/photos/4hbJ-eymZ1o, thanks to Florian Olivo :)
   backgroundImage:
     "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+};
+
+export const DarkMultipleTags = Template.bind({});
+DarkMultipleTags.args = {
+  title: "沒有駭客學校，但你可以自學。",
+  desc: "這堂課程可以讓您從零基礎，搖身一遍成為資安高手。",
+  tags: ["資訊", "資安", "駭客"],
+  // source: https://unsplash.com/photos/4hbJ-eymZ1o, thanks to Florian Olivo :)
+  backgroundImage:
+    "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+};
+
+export const LightSingleTag = Template.bind({});
+LightSingleTag.args = {
+  title: "沒有駭客學校，但你可以自學。",
+  desc: "這堂課程可以讓您從零基礎，搖身一遍成為資安高手。",
+  tags: ["資訊"],
+  // source: https://unsplash.com/photos/fIq0tET6llw, thanks to Diego PH :)
+  backgroundImage:
+    "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+};
+
+export const LightMultipleTags = Template.bind({});
+LightMultipleTags.args = {
+  title: "沒有駭客學校，但你可以自學。",
+  desc: "這堂課程可以讓您從零基礎，搖身一遍成為資安高手。",
+  tags: ["資訊", "資安", "駭客"],
+  // source: https://unsplash.com/photos/fIq0tET6llw, thanks to Diego PH :)
+  backgroundImage:
+    "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
 };
