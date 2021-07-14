ebd2ffd50441ea8ec22381e515d0b14fcc5ca01f
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-14 10:54:51 +0800

    docs(components/Page/HeaderBasePage): done

--- components/Page/HeaderBasePage.tsx
+++ components/Page/HeaderBasePage.tsx
@@ -3,13 +3,45 @@ import React from "react";
 import BasePage from "./BasePage";
 
 export interface HeaderBasePageProps {
+  /**
+   * 頁面 ID
+   */
   id: string;
+  /**
+   * 頁面內容
+   */
   children: ReactNode;
+  /**
+   * 是否將頁面展開到螢幕大小？
+   *
+   * 內部實作：`w-screen h-screen`
+   */
   full?: boolean;
+  /**
+   * 頁面標題
+   */
   title: string;
-  header: ReactNode;
+  /**
+   * 頁面 Jumbotron 部分
+   *
+   * 可放入 React 元件。
+   */
+  jumbotron: ReactNode;
+  /**
+   * 頁面 Jumbotron 的 Class Name
+   *
+   * @default py-12 pt-16 px-8 standard-color place-center
+   */
+  jumbotronClassName: string;
+  /**
+   * 頁面 Jumbotron 的 Style
+   */
+  jumbotronStyle?: React.CSSProperties;
 }
 
+/**
+ * 有 Jumbotron 的基礎頁面。
+ */
 export default function HeaderBasePage({
   id,
   title,
