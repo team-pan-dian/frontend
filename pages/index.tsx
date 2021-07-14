3d1dea7b705b872afe0d9396187a7d04b0215576
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-15 03:09:05 +0800

    feat(pages/index): done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import BasePage from "../components/Page/BasePage";
+import VideoBundleDownloadButton from "../components/OfflineVideo/VideoBundleDownloadButton";
 
 export default function Home() {
   return (
     <BasePage id="home" title="首頁" full>
-      {/* Your Content Here */}
+      <div className="pt-60">
+        <VideoBundleDownloadButton
+          courseId="1"
+          videos={[
+            {
+              id: "25157",
+              url: "https://download.samplelib.com/mp4/sample-15s.mp4",
+            },
+            {
+              id: "25158",
+              url: "https://download.samplelib.com/mp4/sample-15s.mp4",
+            },
+            {
+              id: "25159",
+              url: "https://download.samplelib.com/mp4/sample-15s.mp4",
+            },
+            {
+              id: "25160",
+              url: "https://download.samplelib.com/mp4/sample-15s.mp4",
+            },
+          ]}
+        />
+      </div>
     </BasePage>
   );
 }
