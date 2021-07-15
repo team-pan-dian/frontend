7336d51ac5af4550b25941428fa3d120d6b017ce
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-15 08:48:09 +0800

    feat(pages/course/[cid]/index): done

--- pages/course/[cid]/index.tsx
+++ pages/course/[cid]/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { GetServerSideProps } from "next";
+import Link from "next/link";
 import {
   CourseInfoJumbotron,
   CourseInfoJumbotronClassName,
@@ -9,56 +10,62 @@ import HeaderBasePage from "../../../components/Page/HeaderBasePage";
 import BaseInfoCard from "../../../components/Card/BaseInfoCard";
 import BaseCardsGroup from "../../../components/Card/BaseCardsGroup";
 import BaseButton from "../../../components/Buttons/BaseButton";
+import useCourseData from "../../../components/Fetcher/useCourseData";
+import { CommaTagStringToTags } from "../../../components/Combination/CourseInfoUtil";
+import Message, { MessageLevel } from "../../../components/Combination/Message";
 
 export interface ChooseLessonProps {
   cid: string;
-  title: string;
-  desc: string;
-  tags: string[];
-  backgroundImage: string;
 }
 
-export default function ChooseLesson({
-  // cid,
-  title,
-  desc,
-  tags,
-  backgroundImage,
-}: ChooseLessonProps) {
+export default function ChooseLesson({ cid }: ChooseLessonProps) {
+  const { data, error } = useCourseData(cid);
+
   return (
     <HeaderBasePage
-      id="choose-course"
-      title="選擇課程"
+      id="choose-video"
+      title="選擇影片"
       full
       jumbotron={
         <CourseInfoJumbotron
-          title={title}
-          desc={desc}
-          tags={tags}
-          videos={[
-            {
-              url: "https://download.samplelib.com/mp4/sample-15s.mp4",
-            },
-          ]}
+          title={data?.data.name ?? "正在載入資料⋯⋯"}
+          desc={data?.data.information ?? ""}
+          tags={CommaTagStringToTags(data?.data.type ?? "")}
+          videos={
+            data?.data.videoList.map((video) => ({
+              cid,
+              vid: video.id,
+              url: video.fileName,
+            })) ?? []
+          }
         />
       }
-      jumbotronStyle={CourseInfoJumbotronStyle(backgroundImage)}
+      jumbotronStyle={CourseInfoJumbotronStyle(data?.data.img ?? "")}
       jumbotronClassName={CourseInfoJumbotronClassName}
     >
       <div className="flex justify-center justify-items-center">
         <main>
+          {error && (
+            <Message
+              errorMessage={`無法取得課程資料：${error.message}`}
+              mode={MessageLevel.ERROR}
+            />
+          )}
           <BaseCardsGroup>
-            {new Array(25).fill("怎麼配置環境？").map((value, key) => (
+            {data?.data.videoList.map(({ id: vid, name, information }, key) => (
               <BaseInfoCard
                 className="flex flex-col justify-center justify-items-center p-6"
-                key={`${title}-${value}-${key - 1}`}
+                key={`course-video-${vid}`}
               >
                 <div className="mb-2">
-                  <div className="font-light">CH{key + 1}</div>
-                  <div className="text-xl font-bold">{value}</div>
+                  <div className="font-light text-sm">CH{key + 1}</div>
+                  <div className="text-xl font-bold">{name}</div>
+                  <div>{information}</div>
                 </div>
                 <div>
-                  <BaseButton solid>播放</BaseButton>
+                  <Link href={`/course/${cid}/video/${vid}`}>
+                    <BaseButton solid>播放</BaseButton>
+                  </Link>
                 </div>
               </BaseInfoCard>
             ))}
@@ -73,11 +80,5 @@ export const getServerSideProps: GetServerSideProps<ChooseLessonProps> =
   async ({ query: { cid } }) => ({
     props: {
       cid: typeof cid === "string" ? cid : "unknown",
-      title: "沒有駭客學校，但你可以自學。",
-      desc: "這套課程可以讓您從零基礎，搖身一變成為資安高手。",
-      tags: ["資訊", "資安", "駭客"],
-      // source: https://unsplash.com/photos/4hbJ-eymZ1o, thanks to Florian Olivo :)
-      backgroundImage:
-        "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
     },
   });
