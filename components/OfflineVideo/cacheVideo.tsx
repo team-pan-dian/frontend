e00763151abec4ed905ec9409f8191646dfa0531
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-15 08:49:28 +0800

    feat(components/OfflineVideo/cacheVideo): also cache player

--- components/OfflineVideo/cacheVideo.tsx
+++ components/OfflineVideo/cacheVideo.tsx
@@ -1,4 +1,6 @@
 export interface VideoInfo {
+  cid: string;
+  vid: string;
   url: string;
 }
 
@@ -9,13 +11,20 @@ export interface VideoInfo {
  * @return {Blob} 影片 Blob。
  */
 export async function PrefetchVideoGroup(videos: VideoInfo[]): Promise<null> {
-  await Promise.all(
-    videos.map(async (video) =>
-      fetch(video.url, {
+  await Promise.all([
+    // video
+    ...videos.map(async ({ url }) =>
+      fetch(url, {
         mode: "no-cors",
       })
-    )
-  );
+    ),
+    // player
+    ...videos.map(async ({ cid, vid }) =>
+      fetch(`/course/${cid}/video/${vid}`, {
+        mode: "no-cors",
+      })
+    ),
+  ]);
 
   return null;
 }
